13980d88544273eeb479495fdc5860fe2720d980
JoaoGuilhermeEtec <70458505+JoaoGuilhermeEtec@users.noreply.github.com>
2021-04-13 21:58:48 -0300

    servidorjson

---
+++ INDEX.js
@@ -0,0 +1,15 @@
+const jsonserver = require('json-server');
+
+
+const server = jsonserver.create();
+const router = jsonserver.router('Futurama.json');
+
+const middle = jsonserver.defaults();
+
+const port = process.env.PORT || 3000;
+
+server.use(middle);
+server.use(router);
+server.listen(port, ()=> {
+    console.log(`Servidor rodando na porta http://localhost:${port}`);
+});
